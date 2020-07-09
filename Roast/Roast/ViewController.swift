c1f9fc88914266409a99d6f79a3ee3626292560f
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2020-07-08 21:14:35 -0400

    initial commit

---
+++ Roast/Roast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Roast
+//
+//  Created by Nathan Hedgeman on 7/8/20.
+//  Copyright © 2020 Face2Face Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
